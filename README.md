624ee19d52ec75ae67e093f16d88191c88b30662
David Groves <github@fibrecat.org>
2018-01-31 22:41:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Project Title
+
+A quick exmaple Hangman program for a new Python programmer. I'm not amazing myself, so this isn't likely an amazing canonical example.
+
+## Getting Started
+
+Just run hangman.py.
+
+You can specify the number of lives with the -l or --lives command line option.
+
+## Authors
+
+David Groves.
+
+## Attributions
+
+Wordlist is the Words With Friends wordlist from
+http://www.greenworm.net/sites/default/files/gw-assets/enable1-wwf-v4.0-wordlist.txt
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ hangman.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import random
+import string
+import sys
+import argparse
+
+def show_word(word, already_guessed):
+    '''
+
+    :param word: The word the person is trying to guess.
+    :param already_guessed: A list of the letters they have already attempted to guess.
+    :return: The word to print, with letters gussed printed, and *'s for the unprinted letters.
+    '''
+    output = ""
+    for i in word:
+        if i in already_guessed:
+            output += i
+        else:
+            output += "*"
+    return output
+
+
+def get_input(already_guessed, lives):
+    while True:
+        try:
+            letter = input("Enter a letter to guess. (You have {} lives remaining): ".format(lives))
+            if len(letter) != 1:
+                raise ValueError("You put in nothing, or more than one letter !")
+                break
+            if letter.lower() not in string.ascii_lowercase:
+                raise ValueError("You didn't enter a letter !")
+                break
+            if letter in already_guessed:
+                raise ValueError("You already tried that letter. Pick something new !")
+                break
+        except ValueError as e:
+            print(e)
+        else:
+            return letter
+
+
+def check_win(word, already_guessed):
+    return not set(word).difference(already_guessed)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--lives", help="The number of lives", default="6", type=int)
+    args = parser.parse_args()
+    lives = args.lives
+
+    # Load our wordlist from our file.
+    with open("wordlist.txt") as f:
+        wordlist = f.readlines()
+
+    # Get the random word we will have the person guess. Striping the newline character.
+    word = random.choice(wordlist).strip()
+
+    # Setup a list of what has already been attempted.
+    already_guessed = []
+
+    # Main game loop
+    while lives > 0:
+        print(show_word(word=word, already_guessed=already_guessed))
+        letter = get_input(already_guessed=already_guessed, lives=lives)
+
+        if letter not in word:
+            # You guessed incorrectly.
+            lives -= 1
+
+        # Note you guessed correctly
+        already_guessed.append(letter)
+
+        # Check if you have won.
+        if check_win(word=word, already_guessed=already_guessed):
+            print("Well done, you have won !")
+            print("The word was {}, and you had {} lives remaining !".format(word, lives))
+            sys.exit(0)
+
+    # We only get here if you have lost.
+
+    print("Sorry, but you are out of lives. The word was {}".format(word))
